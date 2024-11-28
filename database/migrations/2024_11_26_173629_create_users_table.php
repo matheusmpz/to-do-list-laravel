374e81ff6222fad58e1da3dae60b0ef5eca62e9c
matheusmpz <matheus.p.soares242@gmail.com>
2024-11-28 13:52:04 -0300

    chore: migrations sent to the database

--- database/migrations/2024_11_26_173629_create_users_table.php
+++ database/migrations/2024_11_26_173629_create_users_table.php
@@ -11,13 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('foto')->nullable();
+            $table->tinyInteger('nivel')->default(0);
             $table->rememberToken();
+            $table->foreignId('plano_id')->constrained('planos');
             $table->timestamps();
         });
 
---
+++ database/migrations/2024_11_26_173630_create_tarefas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->id();
+            $table->string('tarefa', 200);
+            $table->text('descricao');
+            $table->string('prioridade', 10);
+            $table->tinyInteger('status')->default(0); 
+            $table->foreignId('usuario_id')->constrained('usuarios')->onDelete('cascade');
+            $table->datetime('data_tarefa');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tarefas');
+    }
+};
