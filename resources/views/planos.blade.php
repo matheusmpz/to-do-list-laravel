5fe97c917d0ae338bdfab107fa16a1573d18d8df
matheusmpz <matheus.p.soares242@gmail.com>
2024-12-03 15:20:34 -0300

    feat: add HTML structure and two pages to the project

---
+++ resources/views/planos.blade.php
@@ -0,0 +1,50 @@
+@extends("layout.site")
+@section("conteudo")
+<main class="container mt-5 mb-5">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h3 class="m-4">Planos</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero cupiditate, corrupti nam corporis
+                soluta quod sequi doloribus, id vitae, mollitia ab blanditiis tempora ducimus harum officia. Quia
+                sunt earum accusantium.</p>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-md-4 text-center">
+            <h4>Gratuito</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptas nesciunt laudantium pariatur
+                sed sequi placeat atque, suscipit in similique itaque odit fugit laborum aliquid excepturi.
+                Consequatur in voluptates sed.
+            </p>
+            <p>
+                <strong>R$ 0,00</strong>
+            </p>
+            <a href="contratar.html" class="btn btn-primary">Contratar</a>
+        </div>
+        <div class="col-md-4 text-center">
+            <h4>Premium</h4>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, tenetur ipsum architecto atque
+                quo
+                reprehenderit quibusdam labore maxime molestiae ex sapiente, ut veritatis voluptatum rem animi
+                aliquam illum et incidunt.
+            </p>
+            <p>
+                <strong>R$ 29,00</strong>
+            </p>
+            <a href="contratar.html" class="btn btn-primary">Contratar</a>
+        </div>
+        <div class="col-md-4 text-center">
+            <h4>Gold</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quia ab nisi sit ut. Libero
+                quibusdam rerum saepe quis optio, odit dolorum fugiat nesciunt architecto perferendis accusantium
+                corrupti magni qui.
+            </p>
+            <p>
+                <strong>R$ 50,00</strong>
+            </p>
+            <a href=contratar.html" class="btn btn-primary">Contratar</a>
+        </div>
+    </div>
+</main>
+@endsection
---
+++ resources/views/sobre-nos.blade.php
@@ -0,0 +1,13 @@
+@extends("layout.site")
+@section("conteudo")
+<main class="container mt-5 mb-5">
+    <div class="row">
+        <div class="col-12">
+            <h3 class="text-center mt-4">Sobre nós</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nulla voluptas cum, voluptatum
+                ratione in expedita sit laborum incidunt nesciunt quia enim vel, eveniet assumenda perferendis magni
+                exercitationem error quam?</p>
+        </div>
+    </div>
+</main>
+@endsection
